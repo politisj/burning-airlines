ed255b57a955a26f5d5ada37a1077ff4c874c322
AliceYunanTang <tangyunan@hotmail.com>
2017-07-05 13:42:50 +1000

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: "users#new"
+
+  get     '/login' => 'session#new'       # login form
+  post    '/login' => 'session#create'   # check credentials & attempt login (set session)
+  delete  '/login' => 'session#destroy'  # logout (destroy session)
   resources :flights
   resources :airplanes
   resources :users
